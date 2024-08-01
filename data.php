7897991f81ae9308e9f3180906a843ae8524a2d9
Huijie Ren <renh@algonquincollege.com>
2024-08-01 11:55:50 -0400

    Inital

--- data.php
+++ data.php
@@ -1,17 +1,15 @@
-<?php 
-    $dsn = 'mysql:host=localhost;dbname=invoice_manager';
-    $username = "root";
-    $password = "980323";
-  
-    try{
-      $db = new PDO($dsn,$username,$password);
-    }catch(PDOException $e){
-      $error = $e->getMessage();
-      echo $error;
-      exit();
-    }
+<?php
+$dsn = 'mysql:host=localhost;dbname=invoice_manager';
+$username = "root";
+$password = "root";
 
-    $result = $db->query("SELECT * FROM statuses");
-    $statuses = $result->fetchAll(PDO::FETCH_COLUMN,1);
+try {
+  $db = new PDO($dsn, $username, $password);
+} catch (PDOException $e) {
+  $error = $e->getMessage();
+  echo $error;
+  exit();
+}
 
-?>
+$result = $db->query("SELECT * FROM statuses");
+$statuses = $result->fetchAll(PDO::FETCH_COLUMN, 1);
